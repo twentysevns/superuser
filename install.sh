81c351cb91ab2bc0280e39317254a8b64ff1873c
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-01 18:06:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # updating pkg
 clear
-echo 'updating pkg!'
-Sleep 3
+echo ' '
+echo ' '
+echo '  Updating pkg!'
+echo ' '
+echo ' '
+sleep 3
 apt update;clear
 
 # Installing ncurses-utils to avoid eror
-echo 'installing addons to avoid eror...'
-Sleep 3
+echo ' '
+echo '  Installing addons to avoid eror...'
+sleep 3
 apt install ncurses-utils;clear
 
 # Setup sudo
-echo 'setup prefrences'
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 
 # Giving permission
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
-echo 'setup prefrences'
 
 # Remove superuser files
 cd .. && rm -rf superuser
@@ -25,6 +28,6 @@ cd .. && rm -rf superuser
 mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings
 
 # Dramatic scene :D
-echo 'Setup preference and removing files...'
+echo '  Setup Preferences and removing files'
 sleep 3 && exit
 
