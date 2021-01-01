a09629b834c3c453d3489372ce184458fa540ba5
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-01 17:51:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # updating pkg
-apt update
-clear
+echo 'updating pkg'
+Sleep 1
+apt update;clear
 
 # Installing ncurses-utils to avoid eror
 echo 'installing addons to avoid eror...'
-Sleep 3
-apt install ncurses-utils
-clear
+Sleep 2
+apt install ncurses-utils;clear
 
 # Setup sudo
+echo 'setup prefrences'
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 
 # Giving permission
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
+echo 'setup prefrences'
 
 # Remove superuser files
 sudo rm -rf $HOME/superuser
