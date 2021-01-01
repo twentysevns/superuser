7d77ba1c0cb75696bc10577482efe227eb33df00
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-01 16:08:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,13 @@ cat sudo > /data/data/com.termux/files/usr/bin/sudo
 # Giving permission
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
+# Delete Vibration in backspace botton termux!
+cat 0vibrate.txt >> ~/.inputrc
+
+# Delete Vibration in sudo mod in backspace botton termux!
+sudo su
+cat 0vibrate.txt >> ~/.inputrc
+
+# Deleting sudo directory
+rm -rf /data/data/com.termux/files/home/sudo
+exit
