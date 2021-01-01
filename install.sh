221cb64a450c8fcca7f29e42cb058075c61ca4a4
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-01 17:27:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,15 @@ cat sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 clear
 
+# Remove superuser files
+rm -rf /data/data/com.termux/files/home/superuser
+# Dramatic scene :D
+echo 'Removing files...'
+sleep 3
+clear
+sudo
+sleep 5
+
 # add key feature
 mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && exit
 
