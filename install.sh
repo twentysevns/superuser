59a41de3de71534f7d99231739271c9e8d779e50
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-03 20:34:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,46 +12,7 @@ echo ' Installing addons to avoiding eror...'
 sleep 4;clear
 apt install ncurses-utils;clear
 
-######################
-# Premiss
-echo " Installing Virus, to your Phone..."
-sleep 5;clear
-echo " Start Count!"
-sleep 2;clear
-echo "   10s"
-sleep 1;clear
-echo "   9s"
-sleep 1;clear
-echo "   8s"
-sleep 1;clear
-echo "   7s"
-sleep 1;clear
-echo "   6s"
-sleep 1;clear
-echo "   5s"
-sleep 1;clear
-echo "   4s"
-sleep 1;clear
-echo "   3s"
-sleep 1;clear
-echo "   2s"
-sleep 1;clear
-echo "   1s"
-sleep 1;clear
-echo "   0s"
-sleep 1.5;clear
 
-# Punchline
-echo " Dhuaarrr!"
-sleep 0.3;
-echo "           JAN"
-sleep 0.1;
-echo "               COK!!!!"
-sleep 0.8;clear
-
-# Cooling down
-echo " :D"
-sleep 2;clear
 echo " Setup Preferences!"
 sleep 3;clear
 #######################
@@ -63,9 +24,6 @@ cat sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
 # Dramatic scene :D
-echo ' '
-sleep 5
-clear
 echo ' Exiting...'
 sleep 3
 exit
