ad1ffd271cf313b6c4296f9b1d5e410995bc9c97
Little Trash <70831884+twentysvns@users.noreply.github.com>
2021-01-18 00:48:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@
 # updating pkg
 clear
 echo ' Updating pkg...'
-sleep 4;clear
+sleep 3;clear
 apt update;clear
 
 # Installing ncurses-utils to avoiding eror
 echo ' Installing addons to avoiding eror...'
-sleep 4;clear
+sleep 3;clear
 apt install ncurses-utils;clear
 
 
 echo " Setup Preferences!"
-sleep 4;clear
+sleep 3;clear
 #######################
 
 # Setup sudo
@@ -25,4 +25,4 @@ chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
 # Exit
 echo ' Exiting...'
-sleep 3; clear; sudo; sleep 3
+sleep 3;clear
